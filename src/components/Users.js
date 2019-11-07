d43be23bbfc7165011d4056271c8fdfa21ed2201
nick.durbin <nick.durbin@gmail.com>
2019-11-06 21:07:32 -0500

    Update and delte functionality implemented.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import api from "../utils/api"
 
 function Users(props) {
@@ -12,19 +13,34 @@ function Users(props) {
 			.catch(error => {
 				console.log(error)
 			})
-	}, [])
+  }, [])
+  
+  const handleDelete = (e, id) => {
+    e.preventDefault()
+    api().delete(`/users/${id}`)
+      .then(res => {
+        console.log('User was deleted!')
+        setUsers(users.filter(user => user.id !== id))
+      })
+      .catch(err => {
+        console.log(err, err.response)
+      })
+  }
 
 	return (
 		<>
-			<h1>My Account</h1>
-      {users.map(user => (
-        <div key={user.id} className='account'>
-          <div className="account-row">Name: {user.name}</div>
-          <div className="account-row">Email: {user.email}</div>
-        </div>
-      ))}
+			<h1>Users</h1>
+
+			{users.map(user => (
+				<div key={user.id} className="account">
+					<Link className="account-update" to={`/users/${user.id}`}>Edit</Link>
+					<div className='account-delete' onClick={(e) => handleDelete(e, user.id)}>Delete</div>
+					<div className="account-row">Name: {user.name}</div>
+					<div className="account-row">Email: {user.email}</div>
+				</div>
+			))}
 		</>
 	)
 }
 
-export default Users;
+export default Users
